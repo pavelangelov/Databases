e3105b941a385b0393049ae9f0eae348d5107ed3
pavelangelov <p_a_v_el@abv.bg>
2016-11-01 19:17:37 +0200

    Added StudentSystem Data

---
+++ 12.Entity-Framework-Code-First/StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,24 @@
+﻿using StudentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+            : base("SudentSystem")
+        {
+        }
+
+        public IDbSet<Student> Students { get; set; }
+
+        public IDbSet<Course> Courses { get; set; }
+
+        public IDbSet<Homework> Homeworks { get; set; }
+    }
+}
