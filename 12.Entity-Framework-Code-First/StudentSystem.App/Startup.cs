fc0c015a7ed6047d0ffbc7f802e78fac54801a6d
pavelangelov <p_a_v_el@abv.bg>
2016-11-05 12:31:46 +0200

    Refactoring solution

--- 12.Entity-Framework-Code-First/StudentSystem.App/Startup.cs
+++ 12.Entity-Framework-Code-First/StudentSystem.App/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 using StudentSystem.Data;
 using StudentSystem.Models;
-using System.Data.Entity.Migrations;
 
 namespace StudentSystem.App
 {
@@ -13,22 +12,23 @@ namespace StudentSystem.App
         public static void Main()
         {
             var dbContext = new StudentSystemContext();
-            SeedData(dbContext);
 
             // Add single course to DB
             dbContext.Courses.Add(new Course() { Name = "C# Fundamentals", Description = "Learn fundamentals for C#  language." });
             dbContext.SaveChanges();
 
             // Seed some students and homeworks to DB
-            var nextStudentId = dbContext.Students.Count();
-            var students = GenerateStudents(15, nextStudentId);
+            var studentsCount = dbContext.Students.Count();
+            var students = GenerateStudents(15, studentsCount);
             foreach (var st in students)
             {
                 dbContext.Students.Add(st);
             }
 
-            var nextHomeworkId = dbContext.Homeworks.Count();
-            var homeworks = GenerateHomeworks(5, nextHomeworkId);
+            dbContext.SaveChanges();
+
+            var homeworksCount = dbContext.Homeworks.Count();
+            var homeworks = GenerateHomeworks(5, homeworksCount);
             foreach (var homework in homeworks)
             {
                 dbContext.Homeworks.Add(homework);
@@ -70,62 +70,29 @@ namespace StudentSystem.App
 
         }
 
-        public static ICollection<Student> GenerateStudents(int numberOfStudents, int nextId)
+        public static ICollection<Student> GenerateStudents(int numberOfStudents, int studentsCount)
         {
             var result = new List<Student>();
             for (int i = 0; i < numberOfStudents; i++)
             {
-                var name = $"Student {nextId}";
-                var studentNumber = 120 + nextId;
-                result.Add(new Student() { Id = ++nextId,  Name = name, StudentNumber = studentNumber });
+                var name = $"Student {++studentsCount}";
+                var studentNumber = 120 + studentsCount;
+                result.Add(new Student() { Name = name, StudentNumber = studentNumber });
             }
 
             return result;
         }
 
-        public static ICollection<Homework> GenerateHomeworks(int numberOfHomeworks, int nextId)
+        public static ICollection<Homework> GenerateHomeworks(int numberOfHomeworks, int homeworksCount)
         {
             var result = new List<Homework>();
             for (int i = 0; i < numberOfHomeworks; i++)
             {
-                var content = $"Homework #{nextId}";
-                result.Add(new Homework() {Id = ++nextId, Content = content, StudentId = i + 1, CourseId = 1, TimeSent = DateTime.Now.AddDays(i) });
+                var content = $"Homework #{++homeworksCount}";
+                result.Add(new Homework() { Content = content, StudentId = i + 1, CourseId = 1, TimeSent = DateTime.Now.AddDays(i) });
             }
 
             return result;
-        }
-
-        private static void SeedData(StudentSystemContext context)
-        {
-
-            if (context.Courses.Count() == 0)
-            {
-                context.Courses.AddOrUpdate(
-                    new Course() { Id = 1, Name = "Databases", Description = "Learn Relative and NoSQL databases." },
-                    new Course() { Id = 2, Name = "Design Patterns", Description = "Who cares about the name of all patterns." },
-                    new Course() { Id = 3, Name = "HTML Basics", Description = "Learn the fundametals of HTML." });
-                context.SaveChanges();
-            }
-
-            if (context.Students.Count() == 0)
-            {
-                context.Students.AddOrUpdate(
-                    new Student() { Id = 1, Name = "Pavel Angelov", StudentNumber = 111 },
-                    new Student() { Id = 2, Name = "Pesho Peshev", StudentNumber = 112 },
-                    new Student() { Id = 3, Name = "Yana Yankova", StudentNumber = 113 }
-                    );
-                context.SaveChanges();
-            }
-
-            if (context.Homeworks.Count() == 0)
-            {
-                context.Homeworks.AddOrUpdate(
-                    new Homework() { Id = 1, Content = "No time for this homework.", StudentId = 1, CourseId = 1, TimeSent = DateTime.Now },
-                    new Homework() { Id = 2, Content = "See my repo in git pls.", StudentId = 2, CourseId = 1, TimeSent = new DateTime(2016, 10, 29) }
-                );
-                context.SaveChanges();
-            }
-
         }
     }
 }
--- 12.Entity-Framework-Code-First/StudentSystem.Data/Migrations/Configuration.cs
+++ 12.Entity-Framework-Code-First/StudentSystem.Data/Migrations/Configuration.cs
@@ -14,5 +14,38 @@ namespace StudentSystem.Data.Migrations
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = false;
         }
+
+        protected override void Seed(StudentSystemContext context)
+        {
+            if (context.Courses.Count() == 0)
+            {
+                context.Courses.AddOrUpdate(
+                    new Course() { Name = "Databases", Description = "Learn Relative and NoSQL databases." },
+                    new Course() { Name = "Design Patterns", Description = "Who cares about the name of all patterns." },
+                    new Course() { Name = "HTML Basics", Description = "Learn the fundametals of HTML." });
+                context.SaveChanges();
+            }
+
+            if (context.Students.Count() == 0)
+            {
+                context.Students.AddOrUpdate(
+                    new Student() { Name = "Pavel Angelov", StudentNumber = 111 },
+                    new Student() { Name = "Pesho Peshev", StudentNumber = 112 },
+                    new Student() { Name = "Yana Yankova", StudentNumber = 113 }
+                    );
+                context.SaveChanges();
+            }
+
+            if (context.Homeworks.Count() == 0)
+            {
+                context.Homeworks.AddOrUpdate(
+                    new Homework() { Content = "No time for this homework.", StudentId = 1, CourseId = 1, TimeSent = DateTime.Now },
+                    new Homework() { Content = "See my repo in git pls.", StudentId = 2, CourseId = 1, TimeSent = new DateTime(2016, 10, 29) }
+                );
+                context.SaveChanges();
+            }
+
+            base.Seed(context);
+        }
     }
 }
